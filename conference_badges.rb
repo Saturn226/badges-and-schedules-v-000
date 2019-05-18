1e69cfd039306499943bca6432226f850c0b9bde
Ashley Muller <serenehopes@gmail.com>
2019-05-18 06:27:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each do |index, attendee|
+  attendees.collect do |index, attendee|
     "Hello, #{attendee}! you'll be assigned to room #{index}."
   end
 end
